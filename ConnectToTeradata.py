dfd56cf981cec069341e9c11a27da607db480400
Rohan Ahire <rohanahire88@gmail.com>
2018-05-02 11:11:09 -0500

    made a correction in the list

--- ConnectToTeradata.py
+++ ConnectToTeradata.py
@@ -8,11 +8,17 @@ udaExec = teradata.UdaExec (appName="ConnectToTeradata", version="1.0",logConsol
 session = udaExec.connect(method="odbc", system="servername",
 username="xxxxx", password="xxxxx");
 
+#Define empty list
+list = []
+
 #Select sample 10 rows for two columns from table and store the data in a list
 #The session.execute method can execute all queries
 for row in session.execute("SELECT top 10 id,name FROM dbname.tablename", queryTimeout=60):
 	list.append([getattr(row,'id'),getattr(row,'name')])
 
+#print the list created above
+print list
+	
 #Print show table output on screen
 rows = session.execute("SHOW TABLE dbname.tablename")
 for row in rows:
